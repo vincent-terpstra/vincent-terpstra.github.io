c4328fa191410274cc474ad1ccc673ebb21bf1c8
Vincent <vdterpst@gmail.com>
2019-12-09 19:40:24 -0500

    enabled multiple apps

--- js/loader.js
+++ js/loader.js
@@ -6,7 +6,7 @@ window.onload = function(){
 
 function setApp(index){
 	var appIndex = appData[index];
-
+	remove();
 	document.querySelector("#app-title").innerHTML = appIndex.name;
 	document.querySelector("#app-desc").innerHTML = appIndex.desc;
 	
@@ -15,7 +15,7 @@ function setApp(index){
 	script.type="text/javascript"
 	var path = appIndex.path + "/html.nocache.js";
 	script.src = path;
-	
+	script.id = "scriptID";
 	document.querySelector("#embed-html").innerHTML = "";
 	document.body.appendChild(script);
 
@@ -36,60 +36,12 @@ function setApp(index){
               document.getElementById('embed-html').addEventListener('mouseup', handleMouseUp, false);
 }
 
-function filter(type){
-    filterType = type;
-    if(type == ""){
-        // will load the table
-        filterRange(0, Number.MAX_VALUE);
-    } else {
-        loadTableWithFilters();
-    }
+function remove(){
+	var scr = document.getElementById("scriptID");
+	if(scr != null)
+		document.body.removeChild(scr);
+	
+	scr = document.getElementById("html");
+	if(scr != null)
+		document.body.removeChild(scr);
 }
-
-function filterRange(lower, upper){
-    filterAgeMin = lower;
-    filterAgeMax = upper;
-    loadTableWithFilters();
-}
-
-function loadTableWithFilters(){
-    var element = document.querySelector("#main-table-body");
-    element.innerHTML = "";
-    petData.forEach(
-    function (pet)
-        {
-            if((pet.type == filterType || filterType == "") && 
-                (pet.age >= filterAgeMin && pet.age < filterAgeMax)){
-
-                var img = document.createElement("img");
-                var image = pet.image;
-                for(attr in image) //load image attributes
-                    img.setAttribute(attr, image[attr]);
-            
-                var paragraph = document.createElement("p");
-                    paragraph.innerHTML = pet.description;
-                element.appendChild(
-                    cascade("tr", 
-                        cascade("td", img),
-                        cascade("td", 
-                            cascade("h4",   document.createTextNode(pet.name)), 
-                            paragraph, 
-                            cascade("span", document.createTextNode("Age: " + pet.age + " years old."))
-                        )
-                    )
-                )
-            }
-       }
-    );
-}
-
-//Function to load a parent element with children elements
-function cascade(parentTag /*child element(s)*/){
-    var parent = document.createElement(parentTag);
-    var i = 1;
-    while(i < arguments.length){
-        parent.appendChild(arguments[i++]);
-    }
-    return parent;
-}
-
